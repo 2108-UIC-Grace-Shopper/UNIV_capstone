50a6b6beda58187c6c427936741474b222946099
Mason Swager <masonswager@masons-mbp.home>
2021-10-04 13:56:52 -0500

    starting to create the seed

---
+++ db/client.js
@@ -0,0 +1,9 @@
+const {Client} = require("pg")
+
+const connectionString = process.env.DATABASE_URL || "https://localhost:5432/capstone"
+
+const client = new Client({
+    connectionString
+})
+
+modules.export = client
