84748dbda962cc2bde552cb80db37d8b25cf326b
esiah campos <esiah@esiahs-iMac.local>
2021-10-12 10:10:52 -0500

    login solution

--- src/cart.js
+++ src/cart.js
@@ -1,31 +1,31 @@
-import react from "react";
+// import react from "react";
 
-const Cart = () => {
-    return (
-        <div>
-                <h1>Welcome to the Cart!</h1>
-    <div class ="cart-containter">
-      <section class="item1">
-        <img src="https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2016/02/16/1331777047411_1/a-pair-of-mountain-bikers-riding-in-the-dolomites-range-in-north-eastern-italy" height="100px" width="100px"/>
-        <p id="itemDescription" name="item-1" rows="6" cols="50">
-         Description of your purchase here!
-        </p>
-        <sub id="priceRemove">
-        <p class="itemPrice">Price: </p>
-        <button class="RemoveItem">Remove</button>
-        </sub>
-      </section>
+// // const Cart = () => {
+// //     return (
+// //         <div>
+// //                 <h1>Welcome to the Cart!</h1>
+// //     <div class ="cart-containter">
+// //       <section class="item1">
+// //         <img src="https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2016/02/16/1331777047411_1/a-pair-of-mountain-bikers-riding-in-the-dolomites-range-in-north-eastern-italy" height="100px" width="100px"/>
+// //         <p id="itemDescription" name="item-1" rows="6" cols="50">
+// //          Description of your purchase here!
+// //         </p>
+// //         <sub id="priceRemove">
+// //         <p class="itemPrice">Price: </p>
+// //         <button class="RemoveItem">Remove</button>
+// //         </sub>
+// //       </section>
 
       
-    <!-- start of checkout section -->
-    </div>
-      <nav class="checkout-description">
-        <p class="totalPrice">Total: <p>
-        <button type="submit">Checkout</button>
+// //     {/* <!-- start of checkout section --> */}
+// //     </div>
+// //       <nav class="checkout-description">
+// //         <p class="totalPrice">Total: <p>
+// //         {/* <button type="submit">Checkout</button> */}
         
-      </nav>
-        </div>
-    )
-}
+// //       {/* </nav>
+// //         </div> */}
+// //     )
+// // }
 
-export default Cart
+// export default Cart
